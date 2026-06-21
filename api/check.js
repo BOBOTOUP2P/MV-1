0234e9412fff228525ee3a027b75c31a82cce7e9
BOBOTOUP2P <nna617014@gmail.com>
2026-06-21 16:24:58 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
@@ -21,4 +21,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
-}
+};
